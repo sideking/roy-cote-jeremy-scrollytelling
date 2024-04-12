41088f0fcd017957346621fe8121bdbe75417643
sideking <2234847@cmontmorency.qc.ca>
2024-04-11 20:32:49 -0400

    call to action

--- script/script.js
+++ script/script.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
     // gsap code here!
    });
+
+gsap.timeline({repeat: -1})
+.to("#call_to_action", {opacity: 1, duration: 1})
+.to("#call_to_action", {y: 50, duration: 2})
+.to("#call_to_action", {opacity: 0, duration: 2}, "<")
