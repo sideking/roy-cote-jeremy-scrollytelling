4c4364a1e698b84f752333c38f7b0fefbd7ef890
sideking <2234847@cmontmorency.qc.ca>
2024-04-21 22:10:01 -0400

    doing_stuff
    im stuff

--- script/script.js
+++ script/script.js
@@ -3,7 +3,36 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // gsap code here!
    });
 
+
+// call to action
 gsap.timeline({repeat: -1})
 .to("#call_to_action", {opacity: 1, duration: 1})
 .to("#call_to_action", {y: 50, duration: 2})
 .to("#call_to_action", {opacity: 0, duration: 2}, "<")
+
+// spritesheet
+const spriteCloudtain = document.querySelector("#cloudy_meadows");
+const spriteGirlstain = document.querySelector("#seek_girl");
+const busyBody = document.querySelector("body")
+
+if (spriteCloudtain != null) {
+    window.addEventListener("scroll", () => {
+        busyBody.classList.add("is-scrolling");
+        setTimeout(scrollStop, 100);
+    });
+
+    function scrollStop() {
+        busyBody.classList.remove("is-scrolling");
+    }
+};
+
+if (spriteGirlstain != null) {
+    window.addEventListener("scroll", () => {
+        busyBody.classList.add("is-scrolling");
+        setTimeout(scrollStop, 100);
+    });
+
+    function scrollStop() {
+        busyBody.classList.remove("is-scrolling");
+    }
+};
