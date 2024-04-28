07aa268cbbdb919b3304fd65bd5807b3e39b18b2
sideking <2234847@cmontmorency.qc.ca>
2024-04-28 00:20:52 -0400

    alsmost_done_with_the_start

--- script/script.js
+++ script/script.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 // Intro Background
 gsap.timeline({yoyo: true, repeat: -1})
-.to("#arbres_noir",  {x: -1000, duration: 10, ease: "power1.inOut"} )
-.to("#arbres_mauve",  {x: -1500, duration: 10, ease: "power1.inOut"}, "<")
-.to("#arbres_brun",  {x: -2000, duration: 10, ease: "power1.inOut"}, "<")
-.to("#arbres_rouge", {x: -1500, duration: 10, ease: "power1.inOut"}, "<")
+.to("#arbres_noir",  {x: -500, duration: 10, ease: "power1.inOut"} )
+.to("#arbres_mauve",  {x: -800, duration: 10, ease: "power1.inOut"}, "<")
+.to("#arbres_brun",  {x: -1100, duration: 10, ease: "power1.inOut"}, "<")
+.to("#arbres_rouge", {x: -1400, duration: 10, ease: "power1.inOut"}, "<")
 
 // call to action
 gsap.timeline({repeat: -1})
@@ -21,8 +21,8 @@ gsap.timeline({repeat: -1})
 
 
 // spritesheet
-const spriteCloudtain = document.querySelector("#cloudy_meadows");
-const spriteGirltain = document.querySelector("#seek_girl");
+const spriteCloudtain = document.querySelector(".cloudy_meadows");
+const spriteGirltain = document.querySelector(".seek_girl");
 const busyBody = document.querySelector("body")
 
 if (spriteCloudtain != null) {
